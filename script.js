6dcf1331708659939936678f60c062127043be2e
8lliotzhang <8lliotzhang@gmail.com>
2025-07-05 01:16:53 -0400

    am so done, helppe

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 // ok uhh time to lay out the scheme
+//TODO: make certain fields required; amt, payer, min 1 bene.
+//TODO: make outdiv hidden and change this on "done"
+//TODO: make look nice
+//TODO: host
+
 
 const userArray = [1,2,3,4] 
 var channelsArray = [0,0,0,0,0,0]
@@ -16,30 +21,47 @@ const outTextArr = ["2 owes 1", "3 owes 1", "4 owes 1", "3 owes 2", "4 owes 2", 
 //directories would go negative for the opposite direction, cancelling out payments
 //final out would of course adjust this such that it reads 2 owes 1 $5, not 1 owes 2 $-5 thatd be silly
 
+//thx to google AI for this single line
+document.addEventListener('DOMContentLoaded', function() {
+    
+    document.getElementById("formID").addEventListener("submit", function (e) {
+        e.preventDefault();
+        getData(e.target);
+    });
+
+});
 
 
-function checkArrIncludes(inArr){
-    const check = [2,3,4];
-    for (let i = 0; i < inArr.length; i++){
-        if (userArray.includes(inArr[i]) == false){
-            return false;
+function getData(form){
+    var fd = new FormData(form);
+    var bArray = []
+    var fPayer = 0;
+    var fAmt = 0;
+    
+    // formdata --> vars
+    for (var pair of fd.entries()){
+        
+        if (pair[0] == 'usergroup'){
+            bArray.push(pair[1]);
+        }
+        else if (pair[0] == 'payer'){
+            fPayer = pair[1];
+        }
+        else if (pair[0] == 'amt'){
+            fAmt = pair[1];
         }
     }
-    return true;
+
+    console.log("payer: " + fPayer.toString());
+    console.log("b arr: " + bArray);
+    console.log("f amt: " + fAmt);
+    newPay(fAmt, bArray, fPayer);
 }
-
-
 //i know, I know, I KNOW THAT THIS IS TERRIBLE WORDING
 //PAYER in this context paid FIRST
 //BENEFICIARIES are those that got something for free and are now paying back...
  
 function newPay(amt, beneficiary, payer){
-    var goAhead = false;
-
-    if ((userArray.includes(payer)) && (checkArrIncludes)){
-        //console.log("yayyyy yayy it worked")
-        goAhead = true;
-    }
 
     let amtEach = 0;
     amtEach = parseFloat((amt/beneficiary.length).toFixed(2));
@@ -57,8 +79,6 @@ function newPay(amt, beneficiary, payer){
             //still needs like a six part switch case but its just one layer no nesting
             //from there just do a comparator check, which one's bigger
             //from there decide positive or negative yeah
-
-        
             //fun fact! by design this thing doesn't support uhh splitting bills so hopefully that's handled eventually
 
         if (beneficiary[i] !== payer) {
@@ -141,32 +161,36 @@ function newPay(amt, beneficiary, payer){
 
 function finalPrtOut(){
     console.log("__________")
-
     console.log("FINAL OUT:")
 
 //final prtout
-    for (let i = 0; i < channelsArray.length; i++){
+    var finalOut = ""    
+
+for (let i = 0; i < channelsArray.length; i++){
         if (channelsArray[i] == 0){
             //do nothing
         }
         else if (channelsArray[i] >= 0){
-            console.log(outTextArr[i] + ": " + channelsArray[i] + " bucks. ");
+            var a = (outTextArr[i] + ": " + channelsArray[i].toFixed(2) + " bucks. <br>");
+            finalOut = finalOut + a;
         }
         else {
-            console.log(outTextArrInverted[i] + ": " + (-channelsArray[i]) + " bucks. ");
-
+            var b = (outTextArrInverted[i] + ": " + (-channelsArray[i]).toFixed(2) + " bucks. <br>");
+            finalOut = finalOut + b;
         }
-
     }
-
+    document.getElementById("outbox").innerHTML = finalOut;
+    //
+    console.log(finalOut)
 }
     
 
-newPay(30.25, [1,2,3], 1)
-newPay(9.99, [2,3], 1)
-newPay(23,[1],3)
-newPay(99.20,[4,2],1)
+//newPay(30.25, [1,2,3], 1)
+//newPay(9.99, [2,3], 1)
+//newPay(23,[1],3)
+//newPay(99.20,[4,2],1)
 
 //so new pay can effectively handle each transaction and only record the final out. Now for webapp time...
 
-finalPrtOut()
+//finalPrtOut()
+
